364342a0779c0c22e6c0e4bf28412f2130aff070
UglyDonkey <uglyd0nk3y@gmail.com>
2023-02-26 11:33:50 +0100

    ignore empty lines

--- src/immediate.ts
+++ src/immediate.ts
@@ -4,27 +4,37 @@ import { constructLineReader } from "./constructLineReader";
 import { ForwardLineReader } from "./ForwardLineReader";
 import { BackwardLineReader } from "./BackwardLineReader";
 
+interface ImmediateReaderOptions {
+  ignoreEmptyLines?: boolean;
+}
+
 export async function immediateReader(
   file: string | FileHandle,
   LineReaderConstructable: { new(fileHandle: FileHandle): LineReader },
-  count: number
+  count: number,
+  options: ImmediateReaderOptions = {}
 ): Promise<string[]> {
+  const { ignoreEmptyLines } = options;
+
   const reader = await constructLineReader(file, LineReaderConstructable);
 
   const lines: string[] = [];
-  for(let i = 0; i < count && reader.hasNextLine(); i++) {
-    lines.push(await reader.nextLine());
+  while (lines.length < count && reader.hasNextLine()) {
+    const line = await reader.nextLine();
+    if (!ignoreEmptyLines || line) {
+      lines.push(line);
+    }
   }
   await reader.close();
 
   return lines;
 }
 
-export function readFirstLines(file: string | FileHandle, count: number): Promise<string[]> {
-  return immediateReader(file, ForwardLineReader, count);
+export function readFirstLines(file: string | FileHandle, count: number, options?: ImmediateReaderOptions): Promise<string[]> {
+  return immediateReader(file, ForwardLineReader, count, options);
 }
 
 
-export function readLastLines(file: string | FileHandle, count: number): Promise<string[]> {
-  return immediateReader(file, BackwardLineReader, count);
+export function readLastLines(file: string | FileHandle, count: number, options?: ImmediateReaderOptions): Promise<string[]> {
+  return immediateReader(file, BackwardLineReader, count, options);
 }
